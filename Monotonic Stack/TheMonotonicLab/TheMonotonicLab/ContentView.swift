dc1eeb294daa96c91c15859b48f1282817b1d004
BAAPPS <208905508+BAAPPS@users.noreply.github.com>
2026-03-09 13:55:32 -0700

    feat(the monotonic lab): initial commit

---
+++ Monotonic Stack/TheMonotonicLab/TheMonotonicLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheMonotonicLab
+//
+//  Created by D F on 3/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Monotonic Stack/TheMonotonicLab/TheMonotonicLab/TheMonotonicLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMonotonicLabApp.swift
+//  TheMonotonicLab
+//
+//  Created by D F on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct TheMonotonicLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
